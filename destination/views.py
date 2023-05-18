e177d120fc151fd153443e34e635d0e459ab01ce
911-Badea-Dan-Nicolai <daninioknx@yahoo.ro>
2023-05-18 12:24:17 +0300

    Admin requirements are done (take 2)

--- destination/views.py
+++ destination/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, resolve_url
 
 # Create your views here.
 from django.views import View
@@ -6,8 +6,26 @@ from django.views import View
 from destination.models import Destination
 
 
-class GetAllDestinationsView(View):
+class GetPublicDestinationsView(View):
     def get(self, request):
-        destinations = Destination.objects.all()
+        destinations = Destination.objects.filter(is_public=True)
         context = {'destinations': destinations}
         return render(request, 'destination.html', context)
+
+
+class AddPublicDestinationView(View):
+    def get(self, request):
+        return render(request, 'add_public_destination.html')
+
+    def post(self, request):
+        geolocation = request.POST.get('geolocation')
+        title = request.POST.get('title')
+        image = request.POST.get('image')
+        description = request.POST.get('description')
+        is_public = True
+
+        destination = Destination(geolocation=geolocation, title=title, image=image, description=description,
+                                  arrival_date=None, departure_date=None, user_id=None,  is_public=False)
+        destination.save()
+
+        return redirect(resolve_url('/public-destinations/'))
